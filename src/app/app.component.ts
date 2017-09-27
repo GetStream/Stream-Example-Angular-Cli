d55847e13a69a915e1e64f408e40470e6590ae9b
Dwight Gunning <dwight@dwightgunning.com>
2017-09-27 12:37:27 +0200

    Added Foundation for Sites css framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    $(document).foundation();
+  }
+
 }
